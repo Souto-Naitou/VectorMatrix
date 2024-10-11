4ded6a709cc74edfa20d0a5fcaabc805d51c4a92
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-10-12 05:06:00 +0900

    Add License Infomation

--- math/impl/Rect2.cpp
+++ math/impl/Rect2.cpp
@@ -1,3 +1,6 @@
+// Copyright © 2024 Souto-Naitou. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
 #include "Rect2.h"
 
 void Rect2::MakeSquare(int _size, bool _centerMode)
