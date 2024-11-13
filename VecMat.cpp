28d73a2b00dda6cfbf9e15625e9d7ace39632f1e
Souto-Naitou <146906423+Souto-Naitou@users.noreply.github.com>
2024-11-14 00:52:59 +0900

    lib生成できるように変更

--- VecMat.cpp
+++ VecMat.cpp
@@ -5,22 +5,4 @@
 #include <Vector4.h>
 #include "Color.h"
 
-int main()
-{
-    Color color = Vector4();
-
-    color = 0xffffffff;
-    color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-    color.rgba = { 255u, 255u, 255u, 255u };
-
-    unsigned int ucol = color.UInt32();
-    printf("0x%08x\n", ucol);
-
-    Color::RGBA rgba = color.rgba;
-    printf("%d, %d, %d, %d\n", rgba.r, rgba.g, rgba.b, rgba.a);
-
-    Vector4 vec4 = color;
-    printf("%f, %f, %f, %f\n", vec4.x, vec4.y, vec4.z, vec4.w);
-
-    return 0;
-}
+int main(void) { return 0; };
